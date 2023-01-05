897edcbf7677cb730ae53e597d183278babb3f41
Tomasz Rusnak <tomasz.rusnak@dreamstormstudios.com>
2023-01-05 10:37:01 +0100

    add navigation

---
+++ src/styled/containers/NavigationWrapper.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const NavigationWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 5%;
+
+  & h3 {
+    font-size: 2rem;
+  }
+`;
