753f0f974180785ffd168088c9c71acab246045e
Tomasz Rusnak <tomasz.rusnak@dreamstormstudios.com>
2023-01-08 18:12:33 +0100

    add first project

---
+++ src/styled/containers/ProjectsContainerWrapper.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { device } from '../Responsive';
+
+export const ProjectsContainerWrapper = styled.section`
+  position: relative;
+  min-height: 90vh;
+  padding: 5rem 5%;
+`;
